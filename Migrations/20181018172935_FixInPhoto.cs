e9d8aa782aade234d745161453043733c7a6e0c1
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-10-18 19:49:09 +0200

    Add migration to fix Photos
    Migration was created because WordId was added to Photo model

---
+++ Migrations/20181018172935_FixInPhoto.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hereglish.Migrations
+{
+    public partial class FixInPhoto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Words_WordId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WordId",
+                table: "Photos",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Words_WordId",
+                table: "Photos",
+                column: "WordId",
+                principalTable: "Words",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Words_WordId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WordId",
+                table: "Photos",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Words_WordId",
+                table: "Photos",
+                column: "WordId",
+                principalTable: "Words",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
