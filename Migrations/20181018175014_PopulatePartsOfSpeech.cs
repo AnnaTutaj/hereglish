26e1f804fa453eabb7dcdf7aaa3f9466e4a85224
Anna-Komputer\Anna <anna.tutaj@outlook.com>
2018-10-18 19:53:23 +0200

    Populate Parts Of Speech

---
+++ Migrations/20181018175014_PopulatePartsOfSpeech.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hereglish.Migrations
+{
+    public partial class PopulatePartsOfSpeech : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO PartsOfSpeech (Name) VALUES ('Wyrażenie')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM PartsOfSpeech WHERE Name IN ('Wyrażenie')");
+        }
+    }
+}
