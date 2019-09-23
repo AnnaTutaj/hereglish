0bf7ecce84aa565642d718ca69f2b93e15920968
Anna <anna.tutaj@outlook.com>
2019-09-23 21:37:15 +0200

    Add migration creating index to Feature

---
+++ Migrations/20190923192909_AddIndexToFeature.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hereglish.Migrations
+{
+    public partial class AddIndexToFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_ParentId",
+                table: "Features",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Features_Features_ParentId",
+                table: "Features",
+                column: "ParentId",
+                principalTable: "Features",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Features_Features_ParentId",
+                table: "Features");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Features_ParentId",
+                table: "Features");
+        }
+    }
+}
